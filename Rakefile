9788f24efa02d4fa7d62cbd2002512a314b0449b
Todd Evanoff <evanoff@gmail.com>
2018-11-21 11:52:57 -0800

    add tests for testing CI

---
+++ Rakefile
@@ -0,0 +1,9 @@
+# Rakefile
+require 'rake'
+require 'rake/testtask'
+
+Rake::TestTask.new do |t|
+  t.pattern = "tests/**/*_test.rb"
+end
+
+task default: :test
---
+++ tests/hello_test.rb
@@ -0,0 +1,23 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../hello'
+require 'minitest/autorun'
+require 'rack/test'
+
+class MyTest < Minitest::Test
+
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  (1..10).each do |i|
+    define_method("test_app#{i}") do
+      get '/'
+      sleep 1
+      assert last_response.ok?
+    end
+  end
+
+end
